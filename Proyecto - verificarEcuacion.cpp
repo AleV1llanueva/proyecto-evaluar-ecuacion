34dbf91ac73866071fd72d9f38c004a1dec6ed2f
AleV1llanueva <163602565+AleV1llanueva@users.noreply.github.com>
2024-07-31 19:21:06 -0600

    Update - sol. more point - Proyecto - verificarEcuacion.cpp

--- Proyecto - verificarEcuacion.cpp
+++ Proyecto - verificarEcuacion.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 bool verEqu(string ecuation) {
-	int nums = 0, simbs = 0;
+	int nums = 0, simbs = 0; //Contadores de elementos
+	bool isPoint = 0;
 	if (!isdigit(ecuation[0]) || !isdigit(ecuation[ecuation.length() - 1])) { //No termine o inicie en un simbolo que no sea digito
 		cout << "No puede iniciar o terminar asi";
 		return 0;
@@ -18,17 +19,23 @@ bool verEqu(string ecuation) {
 				return 0;
 			}
 			simbs++;
+			isPoint = 0;
 		}
 		else if (ecuation[i] == '.') { //punto
+			if (isPoint) {
+				cout << "Coloco mas de un punto en este numero";
+				return 0;
+			}
 			if (!isdigit(ecuation[i + 1]) || !isdigit(ecuation[i - 1])) {
 				cout << "El punto esta mal colocado";
 				return 0;
 			}
+			isPoint = 1;
 		}
 		else if (isdigit(ecuation[i])) {
 			//numeros
 			if (nums == 0) nums++;
-			else if (nums == 1 && simbs == 1) {
+			else if (nums == simbs) {
 				nums++;
 			};
 		}
@@ -53,6 +60,8 @@ bool verEqu(string ecuation) {
 		cout << "Error, no completa";
 		return 0;
 	}
+	cout << endl;
+	cout << endl;
 	return 1;
 }
 
@@ -61,7 +70,6 @@ int main() {
 	string ecuation;
 	cout << "Ingrese el valor de la ecuacion\n";
 	getline(cin, ecuation);
-	verEqu(ecuation);
 	if (verEqu(ecuation)) cout << "La operacion es correcta";
 	return 0;
 }
